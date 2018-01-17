f7cf292b8c8745539f1086bfb1775a1dbfec441f
prophetss <34379707+prophetss@users.noreply.github.com>
2018-01-18 00:25:51 +0800

    Add files via upload

---
+++ aes.h
@@ -0,0 +1,29 @@
+#ifndef _AES_H_
+#define _AES_H_
+
+/* out buffer length */
+#define AES_BUFSIZ	16
+
+/* key length */
+#define AES_128	16
+#define AES_192	24
+#define AES_256	32
+
+#define AES_SUCCESS	0
+#define AES_ERROR	-1
+
+/* cipher */
+#define AES_CIPHER	1
+/* decipher */
+#define AES_DECIPHER	0
+
+typedef unsigned long size_t;
+
+typedef unsigned char      uint8_t;
+
+extern int aes_data(uint8_t *in, size_t in_len, uint8_t *out, uint8_t *key, size_t key_len, int tupe);
+
+extern int aes_file(const char *in_filename, const char *out_filename, uint8_t *key, size_t key_len, int type);
+
+#endif /* _AES_H_ */
+
